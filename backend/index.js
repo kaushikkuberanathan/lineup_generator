6aa9b5c20863c4a238c3946ed95b90113beed404
Kaushik <kaushikkumar.kuberanathan@cox.com>
2026-03-23 11:38:12 -0400

    fix repo structure - flatten folders

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Lineup Generator API is running');
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
